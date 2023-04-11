9080fc5998b2d30dcfd22b28233932d02011610a
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-04-11 11:15:44 -0300

    Concluindo a validação de dados
    Validando os dados de entrada, e mostrando o alerta de erro caso os dados não seja validos, ou seja, caso os dados não sejam números

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 import { Modal } from './modal.js'
-
+import { AlertError } from './alert-error.js'
 // variáveis - variables
 const form = document.querySelector('form')
 const inputWeight = document.querySelector('#weight')
@@ -14,10 +14,11 @@ form.onsubmit = function (event) {
   const showAlertError = notANumber(weight) || notANumber(height)
 
   if (showAlertError) {
-    console.log('mostrar o alerta de erro')
+    AlertError.open()
     return
   }
 
+  AlertError.close()
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
 
